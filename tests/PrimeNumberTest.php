ffd3d84a91053da771c3218cfa1e6df0928e8639
Stefan Douma <stef@reyez.nl>
2019-01-03 12:34:01 +0100

    Update test

--- tests/PrimeNumberTest.php
+++ tests/PrimeNumberTest.php
@@ -23,4 +23,13 @@ final class PrimeNumberTest extends TestCase
         $this->assertEquals(\PrimeNumbers\PrimeNumber::$NULL, \PrimeNumbers\PrimeNumber::fromNumber(10));
         $this->assertEquals(\PrimeNumbers\PrimeNumber::$NULL, \PrimeNumbers\PrimeNumber::fromNumber(12));
     }
+
+    public function test_should_return_valid_object_for_valid_prime_numbers()
+    {
+        $this->assertEquals(2, \PrimeNumbers\PrimeNumber::fromNumber(2)->getNumber());
+        $this->assertEquals(3, \PrimeNumbers\PrimeNumber::fromNumber(3)->getNumber());
+        $this->assertEquals(5, \PrimeNumbers\PrimeNumber::fromNumber(5)->getNumber());
+        $this->assertEquals(7, \PrimeNumbers\PrimeNumber::fromNumber(7)->getNumber());
+        $this->assertEquals(11, \PrimeNumbers\PrimeNumber::fromNumber(11)->getNumber());
+    }
 }
